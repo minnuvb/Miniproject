d0f09aaf7b4da510bb81114e1845f6af7c7a3480
minnuvb <155319678+minnuvb@users.noreply.github.com>
2024-08-31 16:29:44 +0530

    Update calorie.py

--- calorie.py
+++ calorie.py
@@ -1,24 +1,8 @@
-
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
-from sklearn.model_selection import train_test_split , GridSearchCV
-from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
-from sklearn import metri
-from matplotlib import style
-style.use("seaborn")
-
-from IPython.display import HTML
-import plotly.express as px
-
-import warnings
-warnings.filterwarnings('ignore')
-     
-
+calories = pd.read_csv('calories.csv')
+exercise= pd.read_csv('exercise.csv')
+calories.head()
+exercise.head()
 
 
 
